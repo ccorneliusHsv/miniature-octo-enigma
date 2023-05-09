b342cd1a7a45934b0e19d9aac8532ae9b1274c64
Chris Cornelius <Cornelius>
2023-05-09 14:44:42 -0500

    testing cache

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,17 +1,23 @@
 import {NextResponse} from "next/server";
 import {request} from "@/lib/datocms";
+import {cache} from "react";
 
 const HOMEPAGE_QUERY =
     `query {allOffers {id}}`;
 
 type allOfferReturnType = { allOffers: [{ id: number }] };
 
-export async function GET() {
 
+const getOffers = cache(async ()=>{
     const data = await request({
         query: HOMEPAGE_QUERY,
         variables: {limit: 10}
     }) as allOfferReturnType;
+    return data;
+})
 
+export async function GET() {
+
+const data = getOffers();
     return NextResponse.json(data);
 }
